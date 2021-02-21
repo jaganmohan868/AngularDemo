22f7b518aeba0820b7d158cbc43fc19ac2098e99
jaganmohan868 <jaganmohan868@gmail.com>
2021-02-21 17:04:02 +0530

    First change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   myid = 'Property';
-  title = 'Angular Demo';
+  title = 'Angular Demo for Twoway data binding';
   GetData = (event: any) => console.log(event.target.value);
 }
