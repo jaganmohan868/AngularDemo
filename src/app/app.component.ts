f8fa611d883593da992ef57244b5db34aca40521
jaganmohan868 <jaganmohan868@gmail.com>
2021-02-21 16:58:57 +0530

    First Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  myid = 'Property';
+  title = 'Angular Demo';
+  GetData = (event: any) => console.log(event.target.value);
+}
